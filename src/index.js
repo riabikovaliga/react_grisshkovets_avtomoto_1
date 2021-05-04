9fdde8da88dbbbd3150588c355c8adfd2b31187e
riabikovaliga <riabikova@htmlacademy.ru>
2021-05-04 08:20:37 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+import App from "./App";
+import {createStore, applyMiddleware} from "redux";
+import thunk from "redux-thunk";
+import {rootReducer} from "./store/rootReducer";
+import {Provider} from "react-redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {ActionCreator} from "./store/action";
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+Promise.all([store.dispatch(ActionCreator.addInitialPic())]).then(() => {
+  ReactDOM.render(
+        <Provider store={store}>
+          <App />
+        </Provider>,
+      document.getElementById(`root`)
+  );
+});
